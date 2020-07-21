6ef0771df7b0408b5ea266d65b06370d2d8724c0
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-07-21 21:42:53 +0200

    removing the horrible horror

--- hand.py
+++ hand.py
@@ -33,38 +33,3 @@ class Hand:
         self.turn = turn
         self.river = river
         self.full = full
-
-        self.setup_villains()
-
-    def setup_villains(self):
-        for x, villain in enumerate(self.villains):
-            if villain[0] == -1:
-                break
-
-            before, after, cards = villain
-
-            if x == 0:
-                self.villain_0 = before
-                self.villain_0_after = after
-                if cards:
-                    self.villain_0_hole_cards = cards
-            if x == 1:
-                self.villain_1 = before
-                self.villain_1_after = after
-                if cards:
-                    self.villain_1_hole_cards = cards
-            if x == 2:
-                self.villain_2 = before
-                self.villain_2_after = after
-                if cards:
-                    self.villain_2_hole_cards = cards
-            if x == 3:
-                self.villain_3 = before
-                self.villain_3_after = after
-                if cards:
-                    self.villain_3_hole_cards = cards
-            if x == 4:
-                self.villain_4 = before
-                self.villain_4_after = after
-                if cards:
-                    self.villain_4_hole_cards = cards
