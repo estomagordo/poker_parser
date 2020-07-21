ecfd952c4d526516ef1a03b5bec4b59b4374a5e8
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-07-21 12:26:00 +0200

    can pass hands straight to constructor

--- tournament.py
+++ tournament.py
@@ -1,9 +1,9 @@
 class Tournament:
-    def __init__(self, id, buyin_cents, rake_cents):
+    def __init__(self, id, buyin_cents, rake_cents, hands=[]):
         self.id = id
         self.buyin_cents = buyin_cents
         self.rake_cents = rake_cents
-        self.hands = []
+        self.hands = hands
 
     def add_hand(self, hand):
         self.hands.append(hand)
@@ -12,7 +12,7 @@ class Tournament:
         self.hands.sort(key=lambda hand: hand.time)
         self.start_time = self.hands[0].time
         self.end_time = self.hands[-1].time
-        self.hero_result_cents = 2 * self.buyin_cents * self.did_hero_cash()
+        self.hero_result_cents = 2 * self.buyin_cents * self.did_hero_cash() - self.buyin_cents - self.rake_cents
 
     def did_hero_cash(self):
         last_hand = self.hands[-1]
@@ -20,11 +20,11 @@ class Tournament:
         if last_hand.hero_after:
             return True
 
-        if not any(v == 0 for v in last_hand.villains_after):
+        if not any(v[1] == 0 for v in last_hand.villains):
             return False
 
-        surviving_villain_count = len(v for v in last_hand.villains_after if v)
-        eliminated_villain_stacks = sorted([last_hand.villains[x] for x in range(len(last_hand.villains)) if not last_hand.villains_after[x]])
+        surviving_villain_count = len([v for v in last_hand.villains if v[1]])
+        eliminated_villain_stacks = sorted([last_hand.villains[x][0] for x in range(len(last_hand.villains)) if not last_hand.villains[x][1]])
 
         if surviving_villain_count > 2:
             return False
