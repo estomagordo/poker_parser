bd080b3c273e8f257ae221396237a0a6ca9d69a9
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2020-07-24 00:14:49 +0200

    more tests

--- test_rules.py
+++ test_rules.py
@@ -59,3 +59,73 @@ def test_straights():
     assert(score(broadway) == score(other_broadway))
     assert(score(middling) > score(six_high))
     assert(score(six_high) > score(wheel))
+
+
+def test_trips():
+    trip_queens = 'QdQcQhJd4s'
+    trip_jacks = 'JcJdJhAcKd'
+    other_trip_jacks = 'JhJdJcAc6d'
+    same_other_trip_jacks = 'JsJdJcAs6s'
+
+    assert(score(trip_queens) > score(trip_jacks))
+    assert(score(trip_jacks) > score(other_trip_jacks))
+    assert(score(other_trip_jacks) == score(same_other_trip_jacks))
+
+
+def test_two_pair():
+    aces_up = 'AhAd7c7d4s'
+    other_aces_up = 'AsAc6s6dKc'
+    tens_deuces = 'TdTs2h2c4d'
+    nines_eights = '9c9d8h8s4s'
+    same_nines_eights = '9h9s8d8c4d'
+
+    assert(score(aces_up) > score(other_aces_up))
+    assert(score(aces_up) > score(tens_deuces))
+    assert(score(tens_deuces) > score(nines_eights))
+    assert(score(nines_eights) == score(same_nines_eights))
+
+
+def test_one_pair():
+    jacks = 'JdJc7h3d2h'
+    eights = '8d8hAcKdTs'
+    same_eights = '8s8cAdKsTd'
+
+    assert(score(jacks) > score(eights))
+    assert(score(eights) == score(same_eights))
+
+
+def test_no_hand():
+    royal_sampler = 'AcKdQsJd9h'
+    other_royal_sampler = 'AdKhQhJs9d'
+    meh = 'Jd8h7s4s2h'
+
+    assert(score(royal_sampler) == score(other_royal_sampler))
+    assert(score(royal_sampler) > score(meh))
+
+
+def test_between_hand_types():
+    worst_sflush = 'As2s3s4s5s'
+    best_quads = 'AdAhAcAsKh'
+    worst_quads = '2s2d2h2c3c'
+    best_boat = 'AcAdAhKcKh'
+    worst_boat = '2c2d2h3c3h'
+    best_flush = 'AsKsQsJs9s'
+    worst_flush = '7d5d4d3d2d'
+    best_straight = 'AcKcQdJhTs'
+    worst_straight = 'Ah2h3c4h5d'
+    best_trips = 'AcAdAhKhQd'
+    worst_trips = '2c2d2h3s4h'
+    best_two_pair = 'AhAdKhKsQd'
+    worst_two_pair = '2h2s3h3c4s'
+    best_one_pair = 'AhAcKhQhJd'
+    worst_one_pair = '2h2c3h4d5c'
+    best_high_card = 'AhKcQdJs9d'
+
+    assert(score(worst_sflush) > score(best_quads))
+    assert(score(worst_quads) > score(best_boat))
+    assert(score(worst_boat) > score(best_flush))
+    assert(score(worst_flush) > score(best_straight))
+    assert(score(worst_straight) > score(best_trips))
+    assert(score(worst_trips) > score(best_two_pair))
+    assert(score(worst_two_pair) > score(best_one_pair))
+    assert(score(worst_one_pair) > score(best_high_card))
